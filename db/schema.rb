56d17f2f0284b8567bc3bb1a517b438d14a5fe82
KOTA <kkou0619@gmail.com>
2020-06-06 07:48:30 +0000

    modify prof_image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_06_023143) do
+ActiveRecord::Schema.define(version: 2020_06_06_074348) do
 
   create_table "answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
