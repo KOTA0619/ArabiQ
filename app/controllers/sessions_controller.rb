61051038ced0a0a6ddfbee534b6714c1eb1255df
KOTA <kkou0619@gmail.com>
2020-06-08 23:45:02 +0000

    delete try culculate

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,11 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in(user)
-      #params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      if params[:session][:remember_me] == '1' 
+        remember(user)
+      else
+        forget(user)
+      end
       flash[:success] = 'ログインしました。'
       redirect_to root_url
     else
