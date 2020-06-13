80bb3c4e5c88d6817d38d2c1f7343bf08b8d4d86
KOTA <kkou0619@gmail.com>
2020-06-13 02:33:44 +0000

    activation_mail6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'gmail.com',
+    domain: 'arabiq.herokuapp.com',
     user_name: ENV["kkou0619@gmail.com"],
     password: ENV["qyoetijdijhgtfyg"],
     authentication: 'plain',
