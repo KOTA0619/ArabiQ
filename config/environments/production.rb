8770f0d8a065b0dfc482e1024dd8a741769f4609
KOTA <kkou0619@gmail.com>
2020-06-13 02:21:26 +0000

    activation_mail5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = {host: "kkou0619@gmail.com"}
+  
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -94,12 +94,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.action_mailer.delivery_method = :smtp
+  host = 'arabiq.herokuapp.com'
+  config.action_mailer.default_url_options = {host: host}
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'gmail.com',
-    user_name: 'kkou0619@gmail.com',
-    password: 'qyoetijdijhgtfyg',
+    user_name: ENV["kkou0619@gmail.com"],
+    password: ENV["qyoetijdijhgtfyg"],
     authentication: 'plain',
     enable_starttls_auto: true
   }
