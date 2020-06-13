9656d804f5beb4a27ec4645bc0f439011ef27dfc
KOTA <kkou0619@gmail.com>
2020-06-13 03:21:31 +0000

    activation_mail7

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,8 +100,8 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'arabiq.herokuapp.com',
-    user_name: ENV["kkou0619@gmail.com"],
-    password: ENV["qyoetijdijhgtfyg"],
+    user_name: ENV["GMAIL_ADRESS"],
+    password: ENV["GMAIL_PASSWORD"],
     authentication: 'plain',
     enable_starttls_auto: true
   }
