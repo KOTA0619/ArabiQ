36d1cdfc112f69681740eeb3b9bcf4b9eb4c9b7b
KOTA <kkou0619@gmail.com>
2020-06-21 09:28:25 +0000

    image uplload 3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,6 +11,7 @@ if Rails.env.production?
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: ENV['AWS_REGION'],
+      path_style: true
     }
     config.fog_directory = 'arabiq-image'
     config.fog_attributes = { cache_control: "public, max_age=#{365.days.to_i}"}
